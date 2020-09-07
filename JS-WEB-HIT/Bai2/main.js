b869151a3123bb378fc20137d23f41a355cc97f5
caothethang <55690194+caothethang@users.noreply.github.com>
2020-09-07 19:40:24 +0700

    a

--- JS-WEB-HIT/Bai2/main.js
+++ JS-WEB-HIT/Bai2/main.js
@@ -1,7 +1,8 @@
 var picture = document.getElementsByTagName('img');
 var mainImage = document.getElementById('main');
 for(let i=0;i<picture.length;i++){
-    picture[i].addEventListener('click',function (){
+    picture[i].addEventListener('click',function (ev){
+        console.log(ev.target)
         let imgsrc=picture[i].getAttribute('src');
         mainImage.setAttribute('src',imgsrc);
     })
