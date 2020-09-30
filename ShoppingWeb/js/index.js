3b79f0214a44b17e31db8d95eca7533deb1fc13a
caothethang <55690194+caothethang@users.noreply.github.com>
2020-09-30 10:16:02 +0700

    ShoppingWeb

--- ShoppingWeb/js/index.js
+++ ShoppingWeb/js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+    document.getElementById("search-input-hidden").setAttribute("style", "display:none");
     // hidden-menu
     $("#btn-show-menu").click(function () {
         $(".menu-hidden").animate({ left: "0%" }, "slow");
@@ -16,7 +17,19 @@ $(document).ready(() => {
     $(".cover").click(() => {
         $(".menu-hidden").animate({ left: "-60%" }, "slow");
         $(".cover").animate({ left: "100%" }, "slow");
-        document.getElementById("hidden-cover").setAttribute("style", "display:none !important;")
+        $("#hidden-cover").css("display", "none !important;")
+        $("#search-input-hidden").css("display", "none");
+        $("#search-input-text-hidden").val("");
     })
     // hidden-menu
+    // show-search-input
+    $("#show-search-input").click(function () {
+        $(".cover").animate({ left: "0%" }, "slow");
+        document.getElementById("hidden-cover").setAttribute("style", "display:block !important;")
+        var heightOfPage = $("html").outerHeight();
+        $(".cover").css("height", heightOfPage);
+        $(".cover").css("z-index", "1");
+        $("#search-input-hidden").css("display", "block");
+    })
+    // show-search-input
 })
