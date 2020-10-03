658bc27ec074667edaae8155d509741846333e4d
caothethang <55690194+caothethang@users.noreply.github.com>
2020-10-03 19:34:31 +0700

    Shopping

--- ShoppingWeb/js/product.js
+++ ShoppingWeb/js/product.js
@@ -3,15 +3,17 @@ $(document).ready(function () {
         var id = $(".filter-btn").attr("id");
         var width = $(".filter-hidden-row").outerWidth();
         if (id == "close-btn") {
-            $(".filter-hidden-row").animate({ right: "-40%" });
+            // $(".filter-hidden-row").animate({ right: "-40%" });
+            $(".filter-hidden-row").hide();
             $(".filter-btn").animate({ right: "0%" });
             $(".filter-btn i").removeClass("fas fa-times");
             $(".filter-btn i").addClass("fa fa-filter");
             $(".filter-btn").attr("id", "");
         }
         else {
+            $(".filter-hidden-row").show();
             $(".filter-hidden-row").animate({ right: "0%" });
-            $(".filter-btn").animate({ right: "40%" });
+            $(".filter-btn").animate({ right: width });
             $(".filter-btn i").removeClass("fa fa-filter");
             $(".filter-btn i").addClass("fas fa-times");
             $(".filter-btn").attr("id", "close-btn");
