5e15b14dcf20430e2e296853b6c2471cd8572676
caothethang <55690194+caothethang@users.noreply.github.com>
2020-10-15 08:24:46 +0700

    Shopping

--- ShoppingWeb/js/details.js
+++ ShoppingWeb/js/details.js
@@ -1,27 +1,3 @@
-$(document).ready(function () {
-    $(".filter-btn").click(function () {
-        var id = $(".filter-btn").attr("id");
-        var width = $(".filter-hidden-row").outerWidth();
-        if (id == "close-btn") {
-            // $(".filter-hidden-row").animate({ right: "-40%" });
-            $(".filter-hidden-row").hide();
-            $(".filter-btn").animate({ right: "0%" });
-            $(".filter-btn i").removeClass("fas fa-times");
-            $(".filter-btn i").addClass("fa fa-filter");
-            $(".filter-btn").attr("id", "");
-        }
-        else {
-            $(".filter-hidden-row").show();
-            $(".filter-hidden-row").animate({ right: "0%" });
-            $(".filter-btn").animate({ right: width });
-            $(".filter-btn i").removeClass("fa fa-filter");
-            $(".filter-btn i").addClass("fas fa-times");
-            $(".filter-btn").attr("id", "close-btn");
-        }
-        console.log(width)
-    })
-})
-
 //Get the button
 var mybutton = document.getElementById("myBtn");
 
@@ -50,7 +26,31 @@ function topFunction() {
     }
 }
 
-// 
+
+// đây này
+var btnDes = document.getElementsByClassName("w3-bar-item")
+btnDes[0].addEventListener("click", function () {
+    btnDes[1].style.borderBottom = "none";
+    btnDes[0].style.borderBottom = "2px solid black";
+    btnDes[1].style.color = "#f68e2e";
+    btnDes[0].style.color = "black";
+})
+btnDes[1].addEventListener("click", function () {
+    btnDes[0].style.borderBottom = "none";
+    btnDes[1].style.borderBottom = "2px solid black";
+    btnDes[0].style.color = "#f68e2e";
+    btnDes[1].style.color = "black";
+})
+function openDes(desName) {
+    var i;
+    var x = document.getElementsByClassName("description-product");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    document.getElementById(desName).style.display = "block";
+}
+
+
 $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 10,
@@ -68,7 +68,7 @@ $('.owl-carousel').owlCarousel({
             nav: true
         },
         1000: {
-            items: 5,
+            items: 4,
             nav: true,
             loop: true
         }
--- ShoppingWeb/js/index.js
+++ ShoppingWeb/js/index.js
@@ -33,3 +33,55 @@ $(document).ready(() => {
     })
     // show-search-input
 })
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    try {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+        })
+    }
+    catch (err) {
+        window.scrollTo(0, 0);
+    }
+}
+// 
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    stagePadding: 50,
+    responsiveClass: true,
+    nav: true,
+    navText: ['<i class="fa fa-angle-left fa-3x" aria-hidden="true"></i>', '<i class="fa fa-angle-right fa-3x" aria-hidden="true"></i>'],
+    responsive: {
+        0: {
+            items: 3,
+            nav: true
+        },
+        600: {
+            items: 3,
+            nav: true
+        },
+        1000: {
+            items: 4,
+            nav: true,
+            loop: true
+        }
+    }
+})
